02f85f6468b0e6e8c0a36228aa9ce053f686162a
jjiang43 <jjiang43@illinois.edu>
2019-07-03 17:48:00 +0800

    T53

---
+++ Algorithms/T53_Maximum_Subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int res = nums[0];
+        for(int i = 1; i < n; i++){
+            dp[i] = Math.max(dp[i - 1], 0) + nums[i];
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
