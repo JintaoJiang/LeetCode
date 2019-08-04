51018cfa451b6e39808ae6ee8997b197a7f58393
jjiang43 <jjiang43@illinois.edu>
2019-08-04 12:33:26 +0800

    T12

---
+++ Algorithms/T12_Integer_to_Roman.java
@@ -0,0 +1,10 @@
+class Solution {
+    static String M[] = {"", "M", "MM", "MMM"};
+    static String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    static String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    static String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    
+    public String intToRoman(int num) {
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+    }
+}
